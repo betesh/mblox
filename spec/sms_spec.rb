f1f954c54be81f2ef12c4d5ba687f016c433a0f6
Isaac Betesh <ibetesh@springbig.com>
2013-10-09 10:03:09 -0400

    replaced expect().to with should in sms_spec

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -20,7 +20,7 @@ describe Mblox::Sms do
       number = TEST_NUMBER.to_s
       mblox = Mblox::Sms.new(number,the_message)
       number[1..3] = ''
-      expect(mblox.phone).to eq("1#{TEST_NUMBER}")
+      mblox.phone.should eq("1#{TEST_NUMBER}")
     end
   end
 
@@ -37,7 +37,7 @@ describe Mblox::Sms do
       message = "A"+"ABCDEFGHIJ"*16
       Mblox.config.on_message_too_long = :truncate
       expect { @mblox = Mblox::Sms.new("2"*10, message) }.to_not raise_error
-      expect(@mblox.message).to eq([message[0,160]])
+      @mblox.message.should eq([message[0,160]])
     end
 
     it "cannot be longer than 160 characters if configured to raise error" do
@@ -49,48 +49,48 @@ describe Mblox::Sms do
       message = "ABCDEFGHIJ"*58
       Mblox.config.on_message_too_long = :split
       expect { @mblox = Mblox::Sms.new("2"*10, message) }.to_not raise_error
-      expect(@mblox.message).to eq(["(MSG 1/4): #{message[0,145]}", "(MSG 2/4): #{message[145,145]}", "(MSG 3/4): #{message[290,145]}", "(MSG 4/4): #{message[435,145]}"])
-      expect(@mblox.send).to eq(Array.new(4, result_unroutable))
+      @mblox.message.should eq(["(MSG 1/4): #{message[0,145]}", "(MSG 2/4): #{message[145,145]}", "(MSG 3/4): #{message[290,145]}", "(MSG 4/4): #{message[435,145]}"])
+      @mblox.send.should eq(Array.new(4, result_unroutable))
     end
 
     it "should be split into multiple messages when longer than 160 characters if configured to split and not even split" do
       message = "ABCDEFGHIJ"*32
       Mblox.config.on_message_too_long = :split
       expect { @mblox = Mblox::Sms.new("2"*10, message) }.to_not raise_error
-      expect(@mblox.message).to eq(["(MSG 1/3): #{message[0,145]}", "(MSG 2/3): #{message[145,145]}", "(MSG 3/3): #{message[290..-1]}"])
-      expect(@mblox.send).to eq(Array.new(3, result_unroutable))
+      @mblox.message.should eq(["(MSG 1/3): #{message[0,145]}", "(MSG 2/3): #{message[145,145]}", "(MSG 3/3): #{message[290..-1]}"])
+      @mblox.send.should eq(Array.new(3, result_unroutable))
     end
 
     it "should be safe from changing when short" do
       msg = the_message
       mblox = Mblox::Sms.new(TEST_NUMBER,msg)
       msg[1..3] = ''
-      expect(mblox.message).to eq([the_message])
+      mblox.message.should eq([the_message])
     end
 
     it "should be safe from changing when long" do
       msg = the_message * 10
       mblox = Mblox::Sms.new(TEST_NUMBER,msg)
       msg[1..3] = ''
-      expect(mblox.message[0][11, 20]).to eq(the_message[0,20])
+      mblox.message[0][11, 20].should eq(the_message[0,20])
     end
   end
 
   describe "SMS messages" do
     it "should be sent when the phone number is a Fixnum" do
-      expect(Mblox::Sms.new(TEST_NUMBER.to_i,the_message).send).to eq([result_ok])
+      Mblox::Sms.new(TEST_NUMBER.to_i,the_message).send.should eq([result_ok])
     end
 
     it "should be sent when the phone number is a String" do
-      expect(Mblox::Sms.new(TEST_NUMBER.to_s,the_message).send).to eq([result_ok])
+      Mblox::Sms.new(TEST_NUMBER.to_s,the_message).send.should eq([result_ok])
     end
 
     it "should allow 160-character messages" do
-      expect(Mblox::Sms.new(TEST_NUMBER,"A"*160).send).to eq([result_ok])
+      Mblox::Sms.new(TEST_NUMBER,"A"*160).send.should eq([result_ok])
     end
 
     it "should fail when sent to a landline" do
-      expect(Mblox::Sms.new("6176354500",the_message).send).to eq([result_unroutable])
+      Mblox::Sms.new("6176354500",the_message).send.should eq([result_unroutable])
     end
   end
 end
